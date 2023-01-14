f65afa1a76fc33d1cba82ac3cb0123b35e9cdad0
mhmtmrtyzc <mhmtmrtyzc@gmail.com>
2023-01-14 13:56:54 +0300

    Initial Commit

---
+++ WpCrypto/WpCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WpCryptoApp.swift
+//  WpCrypto
+//
+//  Created by Murat Yazıcı on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WpCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
